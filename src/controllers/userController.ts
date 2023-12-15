bbe2d541f3ec1a8273fb5c34f53b6fd6e71d70d4
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-12-15 00:10:31 -0300

    creating users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,2 +1,37 @@
 import { prisma } from "../database/prisma";
 import { Request, Response } from "express";
+
+export async function createUser(req: Request, res: Response) {
+    const { name, username, email, password } = req.body
+
+    const userExists = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (userExists) {
+        return res.status(400).send('User already exists.')
+    }
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name,
+                username,
+                email,
+                password
+            }
+        })
+
+        return res.status(200).send('User created successfully.')
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+}
+
+export async function listUsers(req: Request, res: Response) {
+    const allUsers = await prisma.user.findMany()
+
+    return res.status(200).send(allUsers)
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { createUser, listUsers } from "../controllers/userController";
 
 const router = Router()
+
+router.post('/user', createUser)
+router.get('/user', listUsers)
 
 export { router }
