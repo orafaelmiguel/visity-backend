2349bd8bebebe67444051c2d70366a9f58563ecc
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-12-16 16:25:26 -0300

    authenticating user

---
+++ src/controllers/loginController.ts
@@ -0,0 +1,30 @@
+import { prisma } from "../database/prisma"
+import { Request, Response } from "express"
+import 'dotenv/config'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { BadRequestError, notFoundError } from "../helpers/apiErrors"
+
+export async function loginUser(req: Request, res: Response) {
+    const { email, password } = req.body
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (!user) throw new BadRequestError('Invalid e-mail or password.')
+
+    const verifyPass = await bcrypt.compare(password, user.password)
+
+    if (!verifyPass) throw new BadRequestError('Invalid e-mail or password.')
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_PASS ?? '', { expiresIn: '24h' })
+    const { password: _, ...userLogin } = user
+
+    return res.status(200).send({
+        user: userLogin,
+        token: token
+    })
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,12 @@
-import { Router } from "express";
-import { signUp, listUsers } from "../controllers/userController";
+import { Router } from "express"
+import { signUp, listUsers } from "../controllers/userController"
+import { loginUser } from "../controllers/loginController"
 
 const router = Router()
 
 router.post('/signup', signUp)
 router.get('/users', listUsers)
+
+router.post('/login', loginUser)
 
 export { router }
