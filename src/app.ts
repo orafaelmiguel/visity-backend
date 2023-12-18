9095c56575bb52f1703737b6bc82429f810dfcf6
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-12-17 23:11:20 -0300

    config cors for frontend connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import 'express-async-errors'
+import cors from 'cors'
 import express from 'express'
 import { router } from './routes/routes'
 import { errorMiddleware } from './middlewares/errorMiddleware'
 
 const app = express()
-
 app.use(express.json())
+app.use(cors({credentials: true}))
 app.use(router)
 app.use(errorMiddleware)
 
