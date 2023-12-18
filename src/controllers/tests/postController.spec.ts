ef9b6c107845f81bb49a77fe5d428ea4201412a3
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-12-18 19:44:23 -0300

    testing post creation with auth token

---
+++ src/controllers/tests/postController.spec.ts
@@ -0,0 +1,22 @@
+import { app } from "../../app";
+import request from "supertest";
+
+describe('Post CRUD', () => {
+    let token: any
+
+    beforeAll(async () => {
+        const res = await request(app).post('/login').send({
+            email: 'email@email.com',
+            password: 'password'
+        });
+        token = res.body.token;
+    });
+
+    it('should be able to create a post', async () => {
+        const res = await request(app)
+            .post('/post')
+            .send({ content: 'content' })
+            .set('authorization', `Bearer ${token}`)
+        .expect(200)
+    })
+})
