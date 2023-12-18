4c4957b7843d5312399c198f7e888227354ec545
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-12-18 19:35:37 -0300

    creating post with auth user

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,2 +1,26 @@
 import { prisma } from "../database/prisma";
 import { Request, Response } from "express";
+
+export async function createPost(req: Request, res: Response) {
+    const { content } = req.body
+    const userId = req.user.id
+
+    const post = await prisma.post.create({
+        data: {
+            content,
+            userId: userId
+        },
+
+        include: {
+            user: {
+                select: {
+                    name: true,
+                    username: true
+                }
+            }
+        }
+
+    })
+
+    return res.status(200).send(post)
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import { loginUser } from "../controllers/loginController"
 import { deleteUsers, listUsers } from "../controllers/userController"
 import { authMiddleware } from "../middlewares/authMiddleware"
 import { getProfile } from "../controllers/profileController"
+import { createPost } from "../controllers/postController"
 
 const router = Router()
 
@@ -15,5 +16,7 @@ router.delete('/users', deleteUsers)
 router.post('/login', loginUser)
 
 router.get('/profile', authMiddleware, getProfile)
+
+router.post('/post', authMiddleware, createPost)
 
 export { router }
