26557b89d8f115e72261e1aad74d448901a10175
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-12-19 20:58:51 -0300

    delete migration

--- prisma/migrations/20231219222126_image_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "images" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "path" TEXT NOT NULL,
-    "postId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "images_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "images_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
