11adee61e9642051641aecc20d058d4a5c15a5b1
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-12-22 00:58:09 -0300

    delete post

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../database/prisma";
 import { Request, Response } from "express";
+import { BadRequestError, UnauthorizedError, notFoundError } from "../helpers/apiErrors";
 
 export async function createPost(req: Request, res: Response) {
     const { content } = req.body
@@ -23,10 +24,32 @@ export async function createPost(req: Request, res: Response) {
                     name: true,
                     username: true
                 }
-            }
+            },
         }
 
     })
 
     return res.status(200).send(post)
+}
+
+export async function deletePost(req: Request, res: Response) {
+    const { postId } = req.params
+    const userId = req.user.id
+
+    const post = await prisma.post.findUnique({
+        where: {
+            id: postId
+        }
+    })
+
+    if(!post) throw new notFoundError('Not Found')
+    if(post.userId !== userId) throw new UnauthorizedError('Unauthorized')
+
+    await prisma.post.delete({
+        where: {
+            id: postId
+        }
+    })
+    
+    return res.status(200).send('Post deleted.')
 }
